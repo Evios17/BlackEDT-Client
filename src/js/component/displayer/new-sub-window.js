6495734672aedc0bad12d9760ace3e652d47f10f
Antoine Doro <antoine.doro@outlook.com>
2023-10-23 21:00:09 +0400

    update

--- src/js/component/displayer/new-sub-window.js
+++ src/js/component/displayer/new-sub-window.js
@@ -1,3 +1,3 @@
 export function newSubWindow(){
-    return window.open("screen2.html", "", "fullscreen=no, scrollbars=auto, width=" + window.screen.availWidth + ", height= " + window.screen.availHeight).moveTo(window.screen.availWidth, window.screen.availHeight);
+    return window.open("screen2.html", "", "fullscreen=no, scrollbars=auto, width=800, height= 800");
 }
